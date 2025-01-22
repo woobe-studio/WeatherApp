5969866e008b09c73fa5173e48b06094e66a2d03
Michael Wozniak <woobe-studio@proton.me>
2025-01-22 06:37:22 +0100

    deleted unnecessary things from map screen

--- app/src/main/java/com/example/weatherapp/presentation/map/MapScreen.kt
+++ app/src/main/java/com/example/weatherapp/presentation/map/MapScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.weatherapp.R
@@ -19,16 +20,8 @@ fun MapScreen(
 ) {
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.map_screen_title)) },
-                navigationIcon = {
-                    IconButton(onClick = navigateToProfile) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = stringResource(id = R.string.back_to_profile)
-                        )
-                    }
-                }
+            MapTopAppBar(
+                navigateToProfile = navigateToProfile
             )
         },
         content = { paddingValues ->
@@ -40,15 +33,34 @@ fun MapScreen(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = stringResource(id = R.string.map_screen_content),
-                    style = MaterialTheme.typography.bodyMedium
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(onClick = navigateToProfile) {
-                    Text(text = stringResource(id = R.string.back_to_profile))
-                }
             }
         }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MapTopAppBar(
+    navigateToProfile: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.map_screen_title),
+                fontWeight = FontWeight.Bold
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = navigateToProfile) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = stringResource(id = R.string.back_to_profile)
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onBackground
+        )
     )
 }
